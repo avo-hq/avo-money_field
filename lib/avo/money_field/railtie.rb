993b7236491d95c0abcafcf06ff2fdd295d92133
Paul Bob <paul.ionut.bob@gmail.com>
2024-09-30 12:37:42 +0300

    refactor: plugin rework

--- lib/avo/money_field/railtie.rb
+++ lib/avo/money_field/railtie.rb
@@ -1,8 +1,12 @@
+require_relative "fields/money_field"
+
 module Avo
   module MoneyField
     class Railtie < Rails::Railtie
       initializer "avo-money_field.init" do
-        Avo.plugin_manager.register Avo::MoneyField::Plugin
+        ActiveSupport.on_load(:avo_boot) do
+          Avo.plugin_manager.register_field :money, Avo::MoneyField::Fields::MoneyField
+        end
       end
     end
   end
